766dc7ad76428f6f33e34a5836ac06fa0cbeb790
zuka-e <52391918+zuka-e@users.noreply.github.com>
2020-04-04 11:56:22 +0900

    Fix issues with scp

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Make symlinks
 (cd ~/dotfiles/; # Inside '()', a Sub-shell works
-linked_files=(.{bash_profile,gitconfig,inputrc,vimrc})
+linked_files=(.{bash_profile,bashrc,gitconfig,inputrc,vimrc})
 for file in ${linked_files[@]}; do
   # Opts(ln): s:symlink f:forced v:verbose
   # If you confirm before it has executed, replace '-f' with '-i'
