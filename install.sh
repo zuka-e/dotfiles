b58cbf60d25423034655dd9c2df26ca77628f78b
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-10-10 00:14:00 +0900

    Revise the indent

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@
 #------------------------------------------------
 # Opts -> s:symlink f:forced v:verbose, i: interactive
 
-(cd ~/dotfiles/; # Inside '()', the Sub-shell works
-linked_files=(.{bash_profile,bashrc,gitconfig,inputrc,tmux.conf,vimrc})
-for file in ${linked_files[@]}; do
-  ln -sfv ~/dotfiles/"${file}" ~/"${file}";
-done;)
+(
+  cd ~/dotfiles/; # Inside '()', the Sub-shell works
+
+  linked_files=(.{bash_profile,bashrc,gitconfig,inputrc,tmux.conf,vimrc})
+  for file in ${linked_files[@]}; do
+    ln -sfv ~/dotfiles/"${file}" ~/"${file}";
+  done;
+)
 
 source ~/.bash_profile
