730217ca618a072245fd48fd5dbb8f2456b23c02
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-10-10 05:39:29 +0900

    Update scripts for the Apple settings

--- macos/install.sh
+++ macos/install.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
+# Run `defaults help` to display the help
+
 #------------------------------------------------
-# System
+# General
 #------------------------------------------------
 
-# Always Show a scroll-bar
-defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+# Always display the scroll-bar
+defaults write -g AppleShowScrollBars -string "Always"
+# When the scrollbar is clicked, jump where clicked
+defaults write -g  AppleScrollerPagingBehavior -bool true
+
+#------------------------------------------------
+# Trackpad
+#------------------------------------------------
+
+# Tap is equivalent to click
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+# Enable transition between applications with four fingers
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2
+# Enable dragging with three fingers
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+# Set the scroll direction to "Natural"
+defaults write -g com.apple.swipescrolldirection -bool true
 
 #------------------------------------------------
 # Finder
@@ -15,12 +32,15 @@ defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 defaults write com.apple.finder QuitMenuItem -bool true
 # Show the fullpath on the title-bar
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# Show all file extensions
+defaults write -g AppleShowAllExtensions -bool true
 # Show all hidden files
 defaults write com.apple.finder AppleShowAllFiles -bool true
 # Not show a warning of a extension-change
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 # Not create icons onto the Desktop
 defaults write com.apple.finder CreateDesktop -bool false
+
 # Reload
 killall Finder
 
@@ -28,12 +48,28 @@ killall Finder
 # Dock
 #------------------------------------------------
 
+# Autohide the Dock when the mouse is out
+defaults write com.apple.dock autohide -bool true
 # Animation-time for showing/hiding the Dock
 defaults write com.apple.dock autohide-time-modifier -float 0
 # Time until the Dock is displayed
-defaults write com.apple.dock autohide-delay -float 0.5 
+defaults write com.apple.dock autohide-delay -float 0.5
+# Not display recent apps
+defaults write com.apple.dock show-recents -bool false
+
+#------------------------------------------------
+# Mission Control
+#------------------------------------------------
+
+# Group windows by application
+defaults write com.apple.dock expose-group-apps -bool true
+# Not rearrange the full-screen applications automatically
+defaults write com.apple.dock mru-spaces -bool false
+
 # Reload
 killall Dock
+
+echo "To reflect the Apple settings, you may need to re-login"
 
 #------------------------------------------------
 # Install Homebrew packages
