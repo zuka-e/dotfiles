06458276b7a8325d96d459d988bb42de18a07503
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-12-01 00:46:44 +0900

    Fix issue Homebrew not upgrading packages if old and new are the same

--- macos/brew.sh
+++ macos/brew.sh
@@ -60,31 +60,32 @@ if [[ ! -z $added_packages ]]; then
 fi
 
 if [[ -z $added_packages && -z $removed_packages ]]; then
-  echo "Already up-to-date."
+  echo -e "All packages are already installed \033[33mbut may be upgraded.\033[0m"
 else
-  echo
-  while true; do
-    read -p "Are you sure to continue? (y/N) " input
-    case $input in
-      y|yes)
-        # Install packages based on `Brewfile`,
-        # and remove those installed with `brew` and not listed in the file.
-        brew bundle -v --cleanup --file="$new_file"
-        # Remove unnecessary dependencies
-        # cf. https://docs.brew.sh/Manpage#autoremove---dry-run
-        brew autoremove
-        # Remove stale lock files, outdated downloads, and caches.
-        # cf. https://docs.brew.sh/Manpage#cleanup-options-formulacask-
-        brew cleanup -vs --prune=all
-        break;;
-      N|no|No|"")
-        echo "Canceled."
-        break;;
-      *)
-        echo "Please answer with y or N.";;
-    esac
-  done
+  echo -e "\033[33mOther packages might also be upgraded.\033[0m"
 fi
+
+while true; do
+  read -p "Are you sure to continue? (y/N) " input
+  case $input in
+    y|yes)
+      # Install packages based on `Brewfile`,
+      # and remove those installed with `brew` and not listed in the file.
+      brew bundle -v --cleanup --file="$new_file"
+      # Remove unnecessary dependencies
+      # cf. https://docs.brew.sh/Manpage#autoremove---dry-run
+      brew autoremove
+      # Remove stale lock files, outdated downloads, and caches.
+      # cf. https://docs.brew.sh/Manpage#cleanup-options-formulacask-
+      brew cleanup -vs --prune=all
+      break;;
+    N|no|No|"")
+      echo "Canceled."
+      break;;
+    *)
+      echo "Please answer with y or N.";;
+  esac
+done
 
 #------------------------------------------------
 # `phpenv`
