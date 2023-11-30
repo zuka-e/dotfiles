0fe5ea02d12dc3ab31602a4220309290a165a151
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-11-30 22:52:01 +0900

    Refactor conditional branching

--- shell/common/utils/ls.sh
+++ shell/common/utils/ls.sh
@@ -9,26 +9,28 @@ if type eza > /dev/null 2>&1; then
   alias llat="ll --time=accessed --sort=accessed"
   alias llmt="ll --time=modified --sort=modified"
   alias llct="ll --time=changed --sort=changed"
-else
-  if is_linux; then
-    time='--time-style="+%Y-%m-%d %H:%M:%S"'
 
-    alias ls="ls --sort=version --almost-all --color=auto"
-    alias ll="ls -all --human-readable $time"
-    alias lli="ll --inode --size"
-    alias llat="llt --sort=time --time=atime"
-    alias llmt="llt --sort=time"
-    alias llct="llt --sort=time --time=ctime"
-  elif is_mac; then
-    time='-D "+%Y-%m-%d %H:%M:%S"'
-
-    alias ls="ls -AG"
-    alias ll="ls -lhT $time"
-    alias lli="ll -is"
-    alias llat="ll -tu"
-    alias llmt="ll -t"
-    alias llct="ll -tc"
-  fi
-
-  unset time
+  return
 fi
+
+if is_linux; then
+  time_style='--time-style="+%Y-%m-%d %H:%M:%S"'
+
+  alias ls="ls --sort=version --almost-all --color=auto"
+  alias ll="ls -all --human-readable $time_style"
+  alias lli="ll --inode --size"
+  alias llat="llt --sort=time --time=atime"
+  alias llmt="llt --sort=time"
+  alias llct="llt --sort=time --time=ctime"
+elif is_mac; then
+  time_style='-D "+%Y-%m-%d %H:%M:%S"'
+
+  alias ls="ls -AG"
+  alias ll="ls -lhT $time_style"
+  alias lli="ll -is"
+  alias llat="ll -tu"
+  alias llmt="ll -t"
+  alias llct="ll -tc"
+fi
+
+unset time_style
