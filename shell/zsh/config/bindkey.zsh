b3d6d75e86775cdfdcb0dfc8f3548c915df8f271
zuka-e <52391918+zuka-e@users.noreply.github.com>
2025-03-18 22:53:47 +0900

    chore: Update Zsh keybindings

--- shell/zsh/config/bindkey.zsh
+++ shell/zsh/config/bindkey.zsh
@@ -1,5 +1,11 @@
 # For details of options, see `man zshzle`.
 # - `bindkey`: lists bound keys
 
+# `^`: ctrl
+# `^[`: esc (meta)
+
+# Default
+bindkey "^[." insert-last-word
+
 bindkey "^L" kill-word
 bindkey "^U" backward-kill-line
