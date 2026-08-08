5cf022825a9f26aaaf28bf4ae78bec5d1c649c1a
zuka-e <52391918+zuka-e@users.noreply.github.com>
2026-08-08 20:40:27 +0900

    fix: Cannot reference shell vars defined in child scripts

--- shell/zsh/lib/oh-my-zsh/index.zsh
+++ shell/zsh/lib/oh-my-zsh/index.zsh
@@ -85,8 +85,17 @@ plugins=(
   ssh
   terraform
   tmux
-  tmux.override
   zsh-syntax-highlighting
 )
+
+# cf. https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux
+ZSH_TMUX_AUTOCONNECT=false
+
+# Start tmux initially
+# cf. https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux
+if [[ -z "$VSCODE_INJECTION" ]] \
+  && [[ "$TERM_PROGRAM" != "WarpTerminal" ]]; then
+  ZSH_TMUX_AUTOSTART=true
+fi
 
 . "$ZSH/oh-my-zsh.sh"
